1e954bcb16e7a1fe654e6f842428b51c8306a8e1
DerickSV <carlosvieira535@gmail.com>
2022-10-25 21:25:33 -0300

    Nova atualização

--- routes/rotaUsuario.js
+++ routes/rotaUsuario.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const router = express.Router();
 
+
+function validaremail() {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+function verificarduplicidade(email){
+    let dadosnovos = [];
+    dadosnovos = usuarios.filter(item=>item.email==email);
+    if(dadosnovos.length>0){
+        return true
+    }
+    return false;
+}
+
 const usuarios=[
     {
         id:1,
         nome:"Derick",
         email:"derick@gmail.com",
-        senha:"12345"
+        senha:"12345678retre"
     },
 
     {
@@ -76,12 +91,35 @@ router.get("/:id",(req,res)=>{
 })
 
 router.post("/",(req,res)=>{
+    let i = 0;
+    let errorMsg = [];
     const {id,nome,email,senha} = req.body;
-    if (nome.length()<=3){
-        res.status(200).send(
-            {mensagem:'Campo nome menor que 3 caracteres'}   
-        ) 
-    }else{
+
+    // Nome
+    if (nome.length<=3){ 
+        i++;
+        errorMsg.push(
+            {mensagem:"Campo nome menor que 3 caracteres"}
+        )
+    }
+
+    // )E-mail
+    if(!validaremail(email)){
+        i++;
+        errorMsg.push(
+            {mensagem:"E-mail inválido"}
+        )
+    }
+
+    // verificarduplicidade
+    if(verificarduplicidade(email)){
+        i++;
+        errorMsg.push(
+            {mensagem:"E-mail já cadastrado"}
+        )
+    }
+
+    if(i==0){
     usuarios.push(       
         {
             id:id,
@@ -91,14 +129,59 @@ router.post("/",(req,res)=>{
         } 
     
     )
-    
     res.status(200).send(
         {mensagem:'Cadastro salvo com sucesso'}   
-    )
-  }
+        )
+    }else{
+        res.status(406),send(
+           {mensagem:errorMsg} 
+        )
+    }
+
+    if (email.length==0){ 
+        res.status(204).send(
+            {mensagem:'Preencher o campo Email'}   
+        ) 
+    }
+
+    if (senha.length<=7){ 
+        res.status(207).send(
+            {mensagem:'Preencher campo Senha'}   
+        ) 
+    }
+    
+    
+  
 });
-router.post("/",(req,res)=>{
-})
 
+router.delete("/",(req,res)=>{
+    let novoarray=[];
+    const {id} = req.body;
+    novoarray=usuarios.filter(linha=>{
+     if(linha.id!==id){
+       return linha;
+     }  
+    })
 
+    res.status(200).send(
+       novoarray
+    )
+   })
+   
+   router.patch("/",(req,res)=>{
+    let novoarray=[];
+     const {nome,email,senha,id} = req.body;
+    novoarray=usuarios.filter(linha=>{
+       if(linha.id==id){
+           return{
+                       id:id,
+                       nome:nome,
+                       email: email,
+                       senha: senha
+                   }
+       }else{
+           return linha;
+       }
+    })
+})     
 module.exports = router;
